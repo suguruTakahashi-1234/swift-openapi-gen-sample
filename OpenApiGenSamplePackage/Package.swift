15d666433d100f8342b6e20cfbeeb7ae1c55793d
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-05-28 11:26:22 +0900

    update

--- OpenApiGenSamplePackage/Package.swift
+++ OpenApiGenSamplePackage/Package.swift
@@ -5,19 +5,31 @@ import PackageDescription
 
 let package = Package(
     name: "OpenApiGenSamplePackage",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "OpenApiGenSamplePackage",
-            targets: ["OpenApiGenSamplePackage"]),
+        .library(name: "OpenApiGenSamplePackage", targets: ["OpenApiGenSamplePackage"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.2.1"),
+        .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.1"),
+        .package(url: "https://github.com/swift-server/swift-openapi-async-http-client", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "OpenApiGenSamplePackage"),
+            name: "OpenApiGenSamplePackage",
+            dependencies: [
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+                .product(name: "OpenAPIAsyncHTTPClient", package: "swift-openapi-async-http-client"),
+            ]
+        ),
         .testTarget(
             name: "OpenApiGenSamplePackageTests",
-            dependencies: ["OpenApiGenSamplePackage"]),
+            dependencies: ["OpenApiGenSamplePackage"]
+        ),
     ]
 )
