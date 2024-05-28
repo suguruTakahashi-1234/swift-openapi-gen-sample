7eff29c36cb85dcd27e1141103628f51ababcc50
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-05-28 10:39:17 +0900

    add project

---
+++ OpenApiGenSample/OpenApiGenSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenApiGenSample
+//
+//  Created by Suguru Takahashi on 2024/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenApiGenSample/OpenApiGenSample/OpenApiGenSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenApiGenSampleApp.swift
+//  OpenApiGenSample
+//
+//  Created by Suguru Takahashi on 2024/05/28.
+//
+
+import SwiftUI
+
+@main
+struct OpenApiGenSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
