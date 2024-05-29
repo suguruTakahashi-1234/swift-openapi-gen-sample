bf2206168ee9589b66a27eda780eb3f5c6b05eaa
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-05-29 11:46:12 +0900

    update

--- OpenApiGenSamplePackage/Sources/OpenApiGenSamplePackage/ContentView.swift
+++ OpenApiGenSamplePackage/Sources/OpenApiGenSamplePackage/ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//  
-//
-//  Created by Suguru Takahashi on 2024/05/28.
-//
-
 import SwiftUI
 import OpenAPIRuntime
 import OpenAPIURLSession
@@ -23,43 +16,88 @@ public struct ContentView: View {
     }
 
     public var body: some View {
-        HStack {
-            Button {
-                Task {
-                    await getUsers()
+        VStack {
+            ScrollView(.horizontal) {
+                HStack {
+                    Button {
+                        Task {
+                            await getUser()
+                            await getUsers()
+                        }
+                    } label: {
+                        VStack {
+                            Image(systemName: "globe")
+                                .imageScale(.large)
+                                .foregroundStyle(.tint)
+                            Text("All")
+                        }
+                        .padding()
+                    }
+                    
+                    
+                    Button {
+                        Task {
+                            await getUsers()
+                        }
+                    } label: {
+                        VStack {
+                            Image(systemName: "globe")
+                                .imageScale(.large)
+                                .foregroundStyle(.tint)
+                            Text("getUsers")
+                        }
+                        .padding()
+                    }
+                    
+                    Button {
+                        Task {
+                            await getUser()
+                        }
+                    } label: {
+                        VStack {
+                            Image(systemName: "globe")
+                                .imageScale(.large)
+                                .foregroundStyle(.tint)
+                            Text("getUser")
+                        }
+                        .padding()
+                    }
                 }
-            } label: {
-                VStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    Text("getUsers")
-                }
-                .padding()
-            }
-            
-            Button {
-                Task {
-                    await getUser()
-                }
-            } label: {
-                VStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundStyle(.tint)
-                    Text("getUser")
-                }
-                .padding()
             }
 
-        }
-
-        List(users) { user in
-            Section {
-                ForEach(getPropertiesAndValues(object: user), id: \.property) { property, value in
-                    Text("\(property): \(value)")
+            List {
+                ForEach(users) { user in
+                    Section {
+                        ForEach(getPropertiesAndValues(of: user), id: \.property) { property, value in
+                            Button {
+                                UIPasteboard.general.string = value
+                            } label: {
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(property)
+                                        .font(.headline)
+                                    Text(value)
+                                        .font(.subheadline)
+                                }
+                            }
+                        }
+                    } header: {
+                        HStack {
+                            Text("User \(user.id)")
+                                .font(.headline)
+                            
+                            Spacer()
+                            
+                            Button {
+                                UIPasteboard.general.string = "\(user)"
+                            } label : {
+                                Image(systemName: "doc.on.doc")
+                            }
+                            .buttonStyle(.borderless)
+                        }
+                    }
                 }
             }
+            .listStyle(.plain)
         }
     }
 
@@ -92,7 +130,7 @@ public struct ContentView: View {
     }
     
     /// 変数名とその値のタプルの配列を返す関数
-    private func getPropertiesAndValues(object: Any) -> [(property: String, value: String)] {
+    private func getPropertiesAndValues(of object: Any) -> [(property: String, value: String)] {
         let mirror = Mirror(reflecting: object)
         return mirror.children.compactMap { child in
             guard let label = child.label else {
@@ -101,9 +139,14 @@ public struct ContentView: View {
             return (label, String(describing: child.value))
         }
     }
+    
+    /// オブジェクト全体を文字列としてコピーするための関数
+    private func getCopyString(from object: Any) -> String {
+        let properties = getPropertiesAndValues(of: object)
+        return properties.map { "\($0.property): \($0.value)" }.joined(separator: "\n")
+    }
 }
 
 #Preview {
     ContentView()
 }
-
